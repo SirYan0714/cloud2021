8d2a75dc2f5d29ff536eae3226af93083c5e4b1d
SirYan0714 <191478244@qq.com>
2021-07-29 20:36:37 +0800

    使用标准ribbon的随机数分配

--- cloud-consumer-order80/src/main/java/com/xtan/mycloud/MainApp80.java
+++ cloud-consumer-order80/src/main/java/com/xtan/mycloud/MainApp80.java
@@ -1,11 +1,14 @@
 package com.xtan.mycloud;
 
+import com.xtan.cusrule.FightRandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration= FightRandomRule.class)
 public class MainApp80 {
     public static void main(String[] args) {
         SpringApplication.run(MainApp80.class,args);
