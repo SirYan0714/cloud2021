0b33a291378e5b5c7b1cf688222e64a4b987d34c
SirYan0714 <191478244@qq.com>
2021-07-31 23:18:12 +0800

    Hystrix 配置完成

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/xtan/mycloud/controller/OrderHystrixController.java
@@ -0,0 +1,40 @@
+package com.xtan.mycloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.xtan.mycloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderHystrixController
+{
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id)
+    {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000")
+    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id)
+    {
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "/(ㄒoㄒ)/调用支付接口超时或异常 或自己异常---我是消费者80：\t"+ "\t当前线程池名字" + Thread.currentThread().getName();
+    }
+}
